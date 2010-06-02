a8c4d7b0f4d80ce4ad06fc958adb08733d5c873b
jevgeni.zelenkov <jevgeni.zelenkov@3554478f-fa96-31d4-2fcb-7149a57cd9f0>
2010-06-02 14:35:44 +0000

    Test Listener fuer SetColor-Button

--- trunk/EasyMoods/src/hm/edu/easymoods/Moods.java
+++ trunk/EasyMoods/src/hm/edu/easymoods/Moods.java
@@ -2,12 +2,24 @@ package hm.edu.easymoods;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class Moods extends Activity {
+	
+	Button setColorButton;	
+	
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        
+        // Listener fuer SetColorButton setzen
+        setColorButton = (Button)findViewById(R.id.SetColorButton);
+        SetButtonListener listener = new SetButtonListener();
+        listener.ctx = this;
+        setColorButton.setOnClickListener(listener);
+        
     }
+    
 }
