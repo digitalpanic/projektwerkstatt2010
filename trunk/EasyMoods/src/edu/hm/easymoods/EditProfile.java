d7f1ed7b9c83d55490519f74aaf4e24426b06108
jevgeni.zelenkov <jevgeni.zelenkov@3554478f-fa96-31d4-2fcb-7149a57cd9f0>
2010-06-21 15:12:59 +0000

    new Profile is working

--- trunk/EasyMoods/src/edu/hm/easymoods/EditProfile.java
+++ trunk/EasyMoods/src/edu/hm/easymoods/EditProfile.java
@@ -1,5 +1,38 @@
 package edu.hm.easymoods;
 
-public class EditProfile {
+import java.io.*;
+import android.app.Activity;
+import android.content.Context;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.RadioButton;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+public class EditProfile extends Activity{
+	private ListView preview;
+	private TextView redValue;
+	private TextView greenValue;
+	private TextView blueValue;
+	private TextView dimValue;
+	private EditText title;
+	private EditText desc;
+	private Button button;
+	private SeekBar seekBar;
+	private String FILENAME = "profiles.txt";
+	
+	
+	 public void onCreate(Bundle savedInstanceState) {
+	        super.onCreate(savedInstanceState);
+	        setContentView(R.layout.modifyprofile);
+	        
+	        TextView caption = (TextView)findViewById(R.id.profileCaption);
+	        caption.setText("EDIT");
+	 }
+
 
 }
